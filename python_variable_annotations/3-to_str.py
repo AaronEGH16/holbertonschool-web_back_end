5fdcddcc19847eda12b5b56d4c93c5d13d31f1b2
AaronEGH16 <aezegonz@gmail.com>
2024-01-02 12:07:03 -0300

    def of to_str function with variable annotations

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+this module contains a definition of "to_str" function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    this function returns a string from float
+    """
+    return str(n)
