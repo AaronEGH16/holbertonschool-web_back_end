1aef958bd1a3cdf69eecd87b82874324e6607cc0
AaronEGH16 <aezegonz@gmail.com>
2024-01-02 10:36:01 -0300

    fixed codestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 this module contains a definition of add method
 """
+
 
 def add(a: float, b: float) -> float:
     """this method returns add of 2 floats"""
