1b75b1f7e35661dce29d2aea10c38c446590b50e
AaronEGH16 <aezegonz@gmail.com>
2024-01-02 10:32:30 -0300

    def of add method with variable annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+this module contains a definition of add method
+"""
+
+def add(a: float, b: float) -> float:
+    """this method returns add of 2 floats"""
+    return a + b
