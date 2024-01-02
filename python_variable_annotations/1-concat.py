ad895314380e479030bbfa42c1e40f933235f811
AaronEGH16 <aezegonz@gmail.com>
2024-01-02 10:56:45 -0300

    def concat function with variable annotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+this module contains a definition of concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    this function returns a concatenated string
+    from 2 strings
+    """
+    return str1 + str2
