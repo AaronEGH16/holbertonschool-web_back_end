71794a2a56a06c86da6a9a884b81a1aef745ba90
AaronEGH16 <aezegonz@gmail.com>
2024-01-03 20:58:41 -0300

    now asyncio await to finish all list element

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,15 +3,18 @@
 this module contains an async function
 """
 
+import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     this function calls "wait_random" function "n"
     times passing max_delay var,
     and returns a sorted list with the awaited "n" times
     """
-    wait_list = [await wait_random(max_delay) for _ in range(n)]
-    wait_list.sort()
-    return wait_list
+    wait_list = [wait_random(max_delay) for _ in range(n)]
+    list_delay = await asyncio.gather(*wait_list)
+    return sorted(list_delay)
