2f015760e5e3cc8e2a2d81ee866a10434bf5221b
AaronEGH16 <aezegonz@gmail.com>
2024-01-04 14:52:16 +0000

    changed type returned desc

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ this module contains a async function
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     this function generate random number 10 times
     """
