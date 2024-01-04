3ca434e2566f1bdbd7ca272fe06e38c4a9d8a391
AaronEGH16 <aezegonz@gmail.com>
2024-01-04 17:38:35 +0000

    I got confused and thought I had to return the average of all the execution times instead of the execution time of all the tasks in an async manner

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,14 +3,15 @@
 this module contains an async function definition
 """
 import asyncio
+from time import time
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
     """this async function calculates measure runtime of
     iterated async function"""
+    start_time = time()
     runtime_list = [async_comprehension() for _ in range(4)]
-    executed = await asyncio.gather(*runtime_list)
-    for x in range(4):
-        executed[x] = sum(executed[x]) / len(executed[x])
-    return sum(executed) / len(executed)
+    await asyncio.gather(*runtime_list)
+    end_time = time()
+    return end_time - start_time
