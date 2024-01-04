239e804e37bec15ac3b498a63ef5fc7fd2d665ba
AaronEGH16 <aezegonz@gmail.com>
2024-01-04 00:03:52 -0300

    function that calculates async execution time

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+this module contains a function
+"""
+import asyncio
+import time
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    This function calculates the average
+    execution time of the "wait_n" async function
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
