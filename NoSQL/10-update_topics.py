f1f002586332aed8cf20adc955ba17d5ee754620
AaronEGH16 <aezegonz@gmail.com>
2024-01-12 14:45:03 -0300

    now update all collection elements with name is equal to name arg

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -9,4 +9,4 @@ def update_topics(mongo_collection, name, topics):
     This function receives a collection from MongoDB, the name field
     and updates its field "topics" by topics arg
     """
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
