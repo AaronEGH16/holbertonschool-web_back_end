24e0beecdd993dd6d5ba305fa00796c8e13665f0
AaronEGH16 <aezegonz@gmail.com>
2024-01-15 17:49:36 -0300

    now uses spread operator to convert all args into array elements

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  var array3 = Array.from(string);
-  return array1.concat(array2, array3);
+  return [...array1, ...array2, ...string];
 }
