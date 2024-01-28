76e8629014dcfbf1bd402357d2e2c32fb76385cc
AaronEGH16 <aezegonz@gmail.com>
2024-01-28 18:42:51 -0300

    i forgotten the return instance

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  Promise
+  return Promise
     .all([uploadPhoto(), createUser()])
     .then((values) => console.log(values[0].body, ...Object.values(values[1])))
     .catch(() => console.log('Signup system offline'));
