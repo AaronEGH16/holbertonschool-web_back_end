c92433643d4b389c8adbcb19bac75595d94488f0
AaronEGH16 <aezegonz@gmail.com>
2024-01-28 18:13:13 -0300

    added then instance

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    .then((res) => (res));
 }
