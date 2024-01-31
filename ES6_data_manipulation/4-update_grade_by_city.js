b434ef842e8c9cbe3015c5ba371e22d84d49879e
AaronEGH16 <aezegonz@gmail.com>
2024-01-30 21:22:43 -0300

    fixed lint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,13 @@
-const updateStudentGradeByCity = (students, city, newGrades) =>
-  students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const newGrade = newGrades.find(
-        (grade) => grade.studentId === student.id
-      );
-      student.grade = newGrade ? newGrade.grade : "N/A";
-      return student;
-    });
+const updateStudentGradeByCity = (students, city, newGrades) => students
+  .filter((student) => student.location === city)
+  .map((student) => {
+    const newGrade = newGrades.find(
+      (grade) => grade.studentId === student.id,
+    );
+    return {
+      ...student,
+      grade: newGrade ? newGrade.grade : 'N/A',
+    };
+  });
 
 export default updateStudentGradeByCity;
